83a6315d8b8df98d5f8216a89a6c48082a634abf
offender <98567050+offender6969@users.noreply.github.com>
2024-03-17 20:04:46 +0530

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class MyModel(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    image = models.ImageField(upload_to='uploads/')
 
     def __str__(self):
         return self.name
